b71c51117ae2565bd2ae1050c988212af7a9878d
Quirogamatias <quirogamatias95@gmail.com>
2022-06-09 11:56:29 -0300

    creacion de bd de heroku

--- sistema/settings/production.py
+++ sistema/settings/production.py
@@ -9,19 +9,13 @@ DEBUG = True
 #ALLOWED_HOSTS = ['277e-179-62-84-59.sa.ngrok.io'] esto se cambia segun la url que te de ngrok, siempre tiene que estar en funcionamiento la aplicacion
 ALLOWED_HOSTS = ['saipes.herokuapp.com']
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd3mu5hlft2vl6s',
+        'USER': 'aetwvpoqrmoozt',
+        'PASSWORD': '448d8e4b7f86458367c55093332cf0c0d666d9c55611697256509a6dfad2cd52',
+        'HOST': 'ec2-54-165-178-178.compute-1.amazonaws.com',
+        'PORT': 5432
     }
-}
-
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   #     'NAME': 'sistema',
-    #    'USER': 'matias',
-     #   'PASSWORD': 'quiroga95',
-      #  'HOST': 'localhost',
-       # 'PORT': 5432
- #   }
